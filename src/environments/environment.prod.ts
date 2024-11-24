6b74151bfb7edb05d2fb498d5cacd8e4cc6af479
Larissa Gomes <larissabeatriz333.lb@gmail.com>
2024-11-24 14:59:56 -0300

    Austes nas cores

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyAWY7YpdZgwzDZtgyhmjw4yF0s2wx5642A",
+    authDomain: "ionic-firebase-crud-1bc4c.firebaseapp.com",
+    projectId: "ionic-firebase-crud-1bc4c",
+    storageBucket: "ionic-firebase-crud-1bc4c.firebasestorage.app",
+    messagingSenderId: "927286087304",
+    appId: "1:927286087304:web:14c90102e6875b7ccdd93d"
+  }
 };
